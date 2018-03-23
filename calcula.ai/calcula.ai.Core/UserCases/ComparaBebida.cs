e71453ee5811396e9f08b0dddf8bb61bd9c99d65
Samuel Diogo <samudiogo@gmail.com>
2018-03-23 19:37:45 -0300

    criação de caso de uso comparativo

---
+++ calcula.ai/calcula.ai.Core/UserCases/ComparaBebida.cs
@@ -0,0 +1,14 @@
+﻿using calcula.ai.Core.Entities;
+
+namespace calcula.ai.Core.UserCases
+{
+    public class ComparaBebida
+    {
+
+        public class MelhorEscolha
+        {
+            public float TotalMl { get; set; }
+            public Bebida BebidaVencedora { get; set; }
+        }
+    }
+}

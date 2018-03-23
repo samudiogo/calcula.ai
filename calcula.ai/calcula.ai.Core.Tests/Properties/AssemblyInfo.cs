bbd57c2b95771770bf1144cd50b59836bb5269b6
Samuel Diogo <samudiogo@gmail.com>
2018-03-23 19:26:25 -0300

    criação de projeto test testando hipóteses da issue #6

---
+++ calcula.ai/calcula.ai.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("calcula.ai.Core.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("calcula.ai.Core.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("12b8223e-a61e-43bf-a4ea-3a700ea45e1e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ calcula.ai/calcula.ai.Core/Entities/Sommelier.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace calcula.ai.Core.Entities
+{
+    public class Sommelier
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
